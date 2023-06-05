ffe45f1ea01c18dbc9a8652f449b6dc049ba8e64
Shreya Singh <66567410+Shreyav23Singh@users.noreply.github.com>
2023-06-05 13:19:29 +0530

    Create Products.js

---
+++ src/components/Products.js
@@ -0,0 +1,92 @@
+export const products =[
+    {
+        id:1,
+        title :"vivo T2 5G (Nitro Blaze, 128 GB)",
+        description: "6GB RAM|128GB ROM ",
+        price :"18999",
+        img :"https://rukminim1.flixcart.com/image/416/416/xif0q/mobile/u/1/h/-original-imagpfbvfu4p55n4.jpeg?q=70" ,
+        quantity :1,
+    },
+    {
+        id:2,
+        title :"Track Pants",
+        description: "Pack of 2 Men Solid Black, Light Blue ",
+        price :"399",
+        img :"https://rukminim1.flixcart.com/image/832/832/l4bn5ow0/track-pant/h/c/l/s-tr10-qfabrix-original-imagf8wfqa8mfbnu.jpeg?q=70" ,
+        quantity :1,
+    },
+    {
+        id:3,
+        title :"Black T-Shirt",
+        description: "Men Solid Round Neck  ",
+        price :"189",
+        img :"https://rukminim1.flixcart.com/image/832/832/xif0q/t-shirt/q/f/c/l-hm-1058black-helmont-original-imaggv48ajehx3rr.jpeg?q=70" ,
+        quantity :1,
+    },
+    {
+        id:4,
+        title :" Sandal",
+        description: "Women Beige Heels ",
+        price :"600",
+        img :"https://rukminim1.flixcart.com/image/832/832/xif0q/sandal/s/u/3/7-tsh-1-beige-jm-looks-beige-original-imagnhkhqbz2p8se.jpeg?q=70" ,
+        quantity :1,
+    },
+    {
+        id:5,
+        title :"3 Seater Sofa",
+        description: "Chilli Billi Fabric-Grey color ",
+        price :"8999",
+        img :"https://rukminim1.flixcart.com/image/416/416/kyxb9u80/sofa-sectional/1/q/k/symmetrical-66-4-grey-jute-187-96-25-tulip-fabric-3-seater-sofa-original-imagbfzmuyjgzrtk.jpeg?q=70" ,
+        quantity :1,
+    },
+    {
+        id:6,
+        title :"FERRERO ROCHER Truffles ",
+        description: "Chocolate ",
+        price :"502",
+        img :"https://rukminim1.flixcart.com/image/416/416/xif0q/chocolate/9/k/u/-original-imagz78ghtxhyvfr.jpeg?q=70" ,
+        quantity :1,
+    },
+    {
+        id:7,
+        title :"APPLE iPhone 14 Pro Max ",
+        description: "Space Black, 128 GB",
+        price :"127999",
+        img :"https://rukminim1.flixcart.com/image/416/416/xif0q/mobile/9/f/p/-original-imaghxenhnpyhn5u.jpeg?q=70" ,
+        quantity :1,
+    },
+    {
+        id:8,
+        title :"Apple AirPods",
+        description: "White, True Wireless ",
+        price :"9999",
+        img :"https://rukminim1.flixcart.com/image/416/416/kpinwy80/headphone/x/r/e/mmef2hn-a-apple-original-imag3qe9hphsevrt.jpeg?q=70" ,
+        quantity :1,
+    },
+    {
+        id:9,
+        title :"Lipstick",
+        description: " Matte Lip Color  ",
+        price :"520",
+        img :"https://rukminim1.flixcart.com/image/416/416/kh0vonk0/lipstick/s/g/u/3-6-9to5-primer-matte-lip-color-mp8-lakme-original-imafx4pzjukgnz65.jpeg?q=70" ,
+        quantity :1,
+    },
+    {
+        id:10,
+        title :"Straight Kurta",
+        description: "Pack of 5 Women Printed Kurta",
+        price :"707",
+        img :"https://rukminim1.flixcart.com/image/832/832/kr2e3680/kurta/x/c/9/xxl-ck-11-15-ketaki-fashion-original-imag4xssh5psnaxr.jpeg?q=70" ,
+        quantity :1,
+    },
+
+
+
+
+
+
+
+
+
+
+];
